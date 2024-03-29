3cbdd090d18b589d1a744e287dc4b3ec3218d053
bbbang105 <2018111366@dgu.ac.kr>
2024-03-29 20:33:48 +0900

    mod week1_0327_3

--- src/week2_0327/전화번호_목록.java
+++ src/week2_0327/전화번호_목록.java
@@ -4,49 +4,46 @@ import java.util.*;
 import java.io.*;
 
 public class 전화번호_목록 {
-    public static void main(String args[]) throws IOException {
+    private static final StringBuilder answer = new StringBuilder();
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
+            String[] phones = new String[N]; // 전화번호를 저장하는 배열
+            Map<String, Integer> map = new HashMap<>();
 
-            List<Long> phones = new ArrayList<>();
             // 전화번호들을 저장
             for (int n = 0; n < N; n++) {
-                phones.add(Long.parseLong(br.readLine()));
+                String phone = br.readLine();
+                phones[n] = phone;
+                map.put(phone, n);
             }
 
-            Collections.sort(phones);
-
-            System.out.println(find(phones));
+            find(phones, map);
         }
+
+        System.out.println(answer);
     }
 
-    private static String find(List<Long> phones) {
-        for (int i = 0; i < phones.size() - 1; i++) {
-            String cur = String.valueOf(phones.get(i));
-            // 본인보다 큰 숫자들만 탐색
-            for (int j = i + 1; j < phones.size(); j++) {
-                String next = String.valueOf(phones.get(j));
-                boolean flag = false;
+    private static void find(String[] phones, Map<String, Integer> map) {
+        StringBuilder sb = new StringBuilder();
+        for (String phone : phones) {
+            // 본인은 제외해야 하므로 : 전체 길이 - 1
+            for (int i = 0; i < phone.length() - 1; i++) {
+                sb.append(phone.charAt(i));
 
-                for (int k = 0; k < cur.length(); k++) {
-                    // 한 자리씩 비교
-                    if (cur.charAt(k) != next.charAt(k)) {
-                        // 통과
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (!flag) {
-                    return "NO";
+                if (map.containsKey(sb.toString())) {
+                    answer.append("NO").append('\n');
+                    return;
                 }
             }
+
+            sb.setLength(0);
         }
 
-        return "YES";
+        answer.append("YES").append('\n');
     }
 }
