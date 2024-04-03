d8cb64f338520fb3656af6c3f2d0012d69ccefb4
bbbang105 <2018111366@dgu.ac.kr>
2024-04-03 11:12:01 +0900

    mod week3_0403_2

--- src/week3_0403/수_찾기.java
+++ src/week3_0403/수_찾기.java
@@ -3,6 +3,8 @@ package week3_0403;
 import java.io.*;
 import java.util.*;
 
+import static java.util.Collections.binarySearch;
+
 public class 수_찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +19,6 @@ public class 수_찾기 {
         }
 
         Collections.sort(A);
-        int maxNum = A.get(A.size() - 1);
 
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
@@ -26,20 +27,12 @@ public class 수_찾기 {
         while (st2.hasMoreTokens()) {
             int num = Integer.parseInt(st2.nextToken());
 
-            if (num > maxNum) {
-                sb.append(0).append('\n');
-                continue;
-            }
+            int id = binarySearch(A, num); // 있다면 0이상
 
-            for (int a : A) {
-                if (a > num) {
-                    sb.append(0).append('\n');
-                    break;
-                }
-                if (a == num) {
-                    sb.append(1).append('\n');
-                    break;
-                }
+            if (id >= 0) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
             }
         }
 
