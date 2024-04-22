a9aad61191a45508bc875a50c6288d2f623fe5ca
bbbang105 <2018111366@dgu.ac.kr>
2024-04-22 11:18:19 +0900

    add week6_0422_1

---
+++ src/week6_0422/피보나치_수.java
@@ -0,0 +1,24 @@
+package week6_0422;
+
+import java.io.*;
+
+public class 피보나치_수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        if (N > 1) {
+            for (int i = 2; i <= N; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
