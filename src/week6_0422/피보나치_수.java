5737fbaa640d3deb1a9fe517ec6390c69a657fd7
bbbang105 <2018111366@dgu.ac.kr>
2024-04-22 11:42:11 +0900

    mod week6_0422_1

--- src/week6_0422/피보나치_수.java
+++ src/week6_0422/피보나치_수.java
@@ -13,10 +13,8 @@ public class 피보나치_수 {
         dp[0] = 0;
         dp[1] = 1;
 
-        if (N > 1) {
-            for (int i = 2; i <= N; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         System.out.println(dp[N]);
