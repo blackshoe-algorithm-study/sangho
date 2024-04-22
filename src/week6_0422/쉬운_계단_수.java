5cbac38873224d8ee971a53137cf94d71dccc47a
bbbang105 <2018111366@dgu.ac.kr>
2024-04-22 16:40:37 +0900

    mod week6_0422_3

--- src/week6_0422/쉬운_계단_수.java
+++ src/week6_0422/쉬운_계단_수.java
@@ -21,21 +21,22 @@ public class 쉬운_계단_수 {
             // j는 i번째 자릿수의 자릿값을 의미함 (0 ~ 9)
             for (int j = 0; j < 10; j++) {
 
-
+                // 0인 경우에는 옆 자리가 1인 경우에만 가능
                 if (j == 0) {
                     dp[i][j] = dp[i - 1][1] % MOD;
                 }
-
+                // 9인 경우에는 옆 자리가 8인 경우에만 가능
                 else if (j == 9) {
                     dp[i][j] = dp[i-1][8] % MOD;
                 }
-
+                // 그 외는 본인 +- 1까지 가능
                 else {
                     dp[i][j] = (dp[i - 1][j - 1] +  dp[i - 1][j + 1]) % MOD;
                 }
             }
         }
 
+        // 최종 값 계산
         int result = 0;
         for (int i = 1; i < 10; i++) {
             result += (dp[N][i] % MOD);
