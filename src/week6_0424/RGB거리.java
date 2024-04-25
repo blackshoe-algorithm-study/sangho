b803bfc767f087ebecd4fcbd1d7390f301134a02
bbbang105 <2018111366@dgu.ac.kr>
2024-04-25 20:10:41 +0900

    mod week6_0424_2

--- src/week6_0424/RGB거리.java
+++ src/week6_0424/RGB거리.java
@@ -21,23 +21,12 @@ public class RGB거리 {
 
         // 최솟값만을 계속 더하기
         for (int i = 1; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                // R을 칠하는 경우
-                if (j == 0) {
-                    // 이전 G or B
-                    dp[i][j] += Math.min(dp[i - 1][j + 1], dp[i - 1][j + 2]);
-                }
-                // G를 칠하는 경우
-                else if (j == 1) {
-                    // 이전 R or B
-                    dp[i][j] += Math.min(dp[i - 1][j - 1], dp[i - 1][j + 1]);
-                }
-                // B를 칠하는 경우
-                else if (j == 2) {
-                    // 이전 R or G
-                    dp[i][j] += Math.min(dp[i - 1][j - 1], dp[i - 1][j - 2]);
-                }
-            }
+            // R을 칠하는 경우, 이전 G or B
+            dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]);
+            // G를 칠하는 경우, 이전 R or B
+            dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]);
+            // B를 칠하는 경우, 이전 R or G
+            dp[i][2] += Math.min(dp[i - 1][0], dp[i - 1][1]);
         }
 
         int result = 100_001;
