657c69ef809d591b21385662774ebada4e6dd74f
bbbang105 <2018111366@dgu.ac.kr>
2024-05-27 16:10:44 +0900

    mod week11_0527_3

--- src/week11_0527/치킨배달.java
+++ src/week11_0527/치킨배달.java
@@ -5,14 +5,18 @@ import java.util.*;
 
 public class 치킨배달 {
     private static List<Set<Integer>> chickenLocations;
+    private static int N, M;
+    private static List<Node> houses;
+    private static List<Node> chickens;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken()); // 선택할 치킨집의 개수
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); // 선택할 치킨집의 개수
 
-        List<Node> houses = new ArrayList<>();
-        List<Node> chickens = new ArrayList<>();
+        houses = new ArrayList<>();
+        chickens = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -26,45 +30,41 @@ public class 치킨배달 {
             }
         }
 
-        int chickenNum = chickens.size() - 1;
+        int result = Integer.MAX_VALUE;
         chickenLocations = new ArrayList<>();
-        createNumberSet(M, chickenNum, new HashSet<>());
+        createNumberSet(0, new HashSet<>());
 
-        int result = 0;
         for (Set<Integer> locations : chickenLocations) {
+            int currentDistance = 0;
             for (Node house : houses) {
-                int houseX = house.x;
-                int houseY = house.y;
-                int minDist = 101;
-
+                int minDist = Integer.MAX_VALUE;
                 for (int location : locations) {
                     int chickenX = chickens.get(location).x;
                     int chickenY = chickens.get(location).y;
 
-                    int dist = Math.abs(houseX - chickenX) + Math.abs(houseY - chickenY);
+                    int dist = Math.abs(house.x - chickenX) + Math.abs(house.y - chickenY);
                     minDist = Math.min(minDist, dist);
                 }
-
-                result += minDist;
+                currentDistance += minDist;
             }
+            result = Math.min(result, currentDistance);
         }
 
         System.out.println(result);
     }
 
-    private static void createNumberSet(int choiceNum, int chickenNum, Set<Integer> set) {
-        if (set.size() == choiceNum) {
-            chickenLocations.add(set);
+    private static void createNumberSet(int start, Set<Integer> set) {
+        if (set.size() == M) {
+            chickenLocations.add(new HashSet<>(set));
+            return;
         }
 
-        for (int i = 0; i <= chickenNum; i++) {
-            if (set.add(i)) {
-                set.add(i);
-                createNumberSet(choiceNum, chickenNum, set);
-            }
+        for (int i = start; i < chickens.size(); i++) {
+            set.add(i);
+            createNumberSet(i + 1, set);
+            set.remove(i);
         }
     }
-
 
     static class Node {
         int x;
