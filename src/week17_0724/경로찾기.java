4689d4474e26d96d7c19863d5b35d174862e73f7
bbbang105 <2018111366@dgu.ac.kr>
2024-07-24 12:05:47 +0900

    add week17_0724_1

---
+++ src/week17_0724/경로찾기.java
@@ -0,0 +1,40 @@
+package week17_0724;
+
+import java.io.*;
+import java.util.*;
+
+public class 경로찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (dp[i][j] == 0 && dp[i][k] != 0 && dp[k][j] != 0) {
+                        dp[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(dp[i][j]).append(" ");
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
