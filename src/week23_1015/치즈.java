0a4ab7cfe9a0602ee6434545f4a2fc1611f91af0
bbbang105 <2018111366@dgu.ac.kr>
2024-10-15 12:24:35 +0900

    add week23_1015_4

---
+++ src/week23_1015/치즈.java
@@ -0,0 +1,26 @@
+package week23_1015;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 치즈 {
+    private static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
